d1ead79210672ba6eb1bc7b3f34712f1dbdec30e
katepars <katelyn.parsons@edu.sait.ca>
2024-12-08 18:14:06 -0700

    updates

---
+++ backup.js
@@ -0,0 +1,9 @@
+// Select the button
+const button = document.getElementById('projects');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+    // Redirect to another page within the website
+    window.location.href = 'project.html'; // Replace with the relative path to your page
+});
+
