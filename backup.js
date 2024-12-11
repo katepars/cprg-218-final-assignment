51458ff1fb9ed53a3d06269a4631c11f460ced09
katepars <katelyn.parsons@edu.sait.ca>
2024-12-11 15:27:58 -0700

    ..

--- backup.js
+++ backup.js
@@ -8,7 +8,16 @@ button.addEventListener('click', function() {
 });
 
 
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('show');
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}))
